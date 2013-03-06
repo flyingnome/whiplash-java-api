3e4183f6bfee4bcf3f5336034aac12ffee1c576e
Aaron Henshaw <aaronhenshaw@gmail.com>
2013-03-06 16:33:22 -0500

    api no ssl

--- src/com/grandst/whiplash/api/API.java
+++ src/com/grandst/whiplash/api/API.java
@@ -2,10 +2,24 @@ package com.grandst.whiplash.api;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -14,11 +28,19 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.GsonBuilder;
@@ -29,9 +51,10 @@ import com.grandst.whiplash.Whiplash;
 public final class API {
 	
 	public static String get(String apiCall, Whiplash w) throws ClientProtocolException, IOException {
-		HttpClient client = new DefaultHttpClient();
+		HttpClient client = getNewHttpClient(null);
 		HttpGet getReq = new HttpGet(w.getApiBaseUrl()+apiCall);
 		setHeaders(getReq, w);
+		
 		final HttpResponse response = client.execute(getReq);  
 		InputStream inputStream;
 		inputStream = response.getEntity().getContent();		
@@ -42,7 +65,7 @@ public final class API {
 		HttpParams httpParameters = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-		HttpClient client = new DefaultHttpClient(httpParameters); 	
+		HttpClient client =  getNewHttpClient(httpParameters); 	
 		HttpPost postReq = new HttpPost(w.getApiBaseUrl()+apiCall);
 		if (postData != null) {
 			HashMap<String,String> map = new HashMap<String,String>();
@@ -64,7 +87,7 @@ public final class API {
 		HttpParams httpParameters = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-		HttpClient client = new DefaultHttpClient(httpParameters); 	
+		HttpClient client =  getNewHttpClient(httpParameters); 	 	
 		HttpPost postReq = new HttpPost(w.getApiBaseUrl()+apiCall);
 		if (jsonObj != null) {
 			postReq.setEntity(jsonObj);
@@ -93,7 +116,7 @@ public final class API {
 		HttpParams httpParameters = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-		HttpClient client = new DefaultHttpClient(httpParameters); 	
+		HttpClient client =  getNewHttpClient(httpParameters); 	 	
 		HttpPut putReq = new HttpPut(w.getApiBaseUrl()+apiCall);
 		if (jsonObj != null) {
 			putReq.setEntity(jsonObj);
@@ -108,7 +131,7 @@ public final class API {
 		HttpParams httpParameters = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-		HttpClient client = new DefaultHttpClient(httpParameters); 
+		HttpClient client =  getNewHttpClient(httpParameters); 	 
 		HttpDelete delReq = new HttpDelete(w.getApiBaseUrl()+apiCall);
 		setHeaders(delReq,w);
 		final HttpResponse response = client.execute(delReq);  
@@ -136,6 +159,62 @@ public final class API {
 		req.setHeader("X-API-VERSION","1");
 		//use later
 		//req.setHeader("X-API-VERSION",API_KEY);
+	}
+	
+	public static HttpClient getNewHttpClient(HttpParams params) {
+	    try {
+	        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+	        trustStore.load(null, null);
+
+	        SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+	        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+	        if (params == null)
+	        	params = new BasicHttpParams();
+	        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+	        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+	        SchemeRegistry registry = new SchemeRegistry();
+	        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+	        registry.register(new Scheme("https", sf, 443));
+
+	        ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+	        return new DefaultHttpClient(ccm, params);
+	    } catch (Exception e) {
+	        return new DefaultHttpClient();
+	    }
+	}
+	
+	public static class MySSLSocketFactory extends SSLSocketFactory {
+	    SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	    public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+	        super(truststore);
+
+	        TrustManager tm = new X509TrustManager() {
+	            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+	        };
+
+	        sslContext.init(null, new TrustManager[] { tm }, null);
+	    }
+
+	    @Override
+	    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+	        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+	    }
+
+	    @Override
+	    public Socket createSocket() throws IOException {
+	        return sslContext.getSocketFactory().createSocket();
+	    }
 	}
 
 }
